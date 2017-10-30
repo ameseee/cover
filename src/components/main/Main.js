695b3bd54d78f004cbd9fe66f0d59dccd2ab2c36
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-10-30 17:09:14 -0600

    Render contacts on main page from firebase, with approp card styles

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,37 +1,47 @@
 import React from 'react';
+import fetchScopedUsers from './../../utils/fetchScopedUsers';
+import firebase from './../../firebase';
 // import ContactCards from '../contactCards/ContactCards';
+class Main extends React.Component {
+  constructor() {
+    super();
 
-const Main = (props) => {
-console.log(props);
-  return (
-    <div className="main">
-      <main>
-        <section className="emergency-section">
-          <h3 className="emergency-title">Emergency</h3>
-          <button className="emergency-btn">TEXT 911</button>
-        </section>
+    this.fetchScopedUsers = fetchScopedUsers.bind(this);
+  }
 
-        <section className="contacts-section">
-          <h3 className="section-title">Contacts</h3>
+  componentDidMount() {
+    this.fetchScopedUsers(firebase);
+  }
 
-          <article className="contact-card">
-            <h4 className="contact-card-name">hard coded friend1</h4>
-            <button className="custom-text-btn">send custom text</button>
-            <button className="location-now-btn">send my location NOW</button>
-          </article>
+  loadContacts() {
+    return this.props.loadedContacts.map((contact) => {
+      return (
+        <article className="contact-card">
+          <h4 className="contact-card-name">{contact.contactName}</h4>
+          <button className="custom-text-btn">Send custom text</button>
+          <button className="location-now-btn">Send my location NOW!!</button>
+        </article>
+      )
+    })
+  }
 
-          <article className="contact-card">
-            <h4 className="contact-card-name">hard coded friend2</h4>
-            <button className="custom-text-btn">send custom text</button>
-            <button className="location-now-btn">send my location NOW</button>
-          </article>
+  render() {
+    return (
+      <div className="main">
+        <main>
+          <section className="emergency-section">
+            <h3 className="emergency-title">Emergency</h3>
+            <button className="emergency-btn">TEXT 911</button>
+          </section>
 
-{/* dont i want to go through all the contacts in firebase, filter out the ones who have the currentUserId === userId, then make a card for each other those? */}
-
-        </section>
-      </main>
-    </div>
-  );
-};
+          <section className="contacts-section">
+            <h3 className="section-title">Contacts</h3>
+            {this.loadContacts()}
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
 
 export default Main;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 import Main from '../components/main/Main';
+import { loadContacts } from '../actions/actions';
 
 const mapStateToProps = store => ({
   cards: store.cards,
-  currentUser: store.currentUser
+  loadedContacts: store.loadContacts,
+  currentUser: store.currentUser,
 });
 
-// const mapDispatchToProps = dispatch => ({
-//
-// });
+const mapDispatchToProps = dispatch => ({
+  loadContacts: contacts => {
+    dispatch(loadContacts(contacts));
+  },
+});
 
-export default connect(mapStateToProps, undefined)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
