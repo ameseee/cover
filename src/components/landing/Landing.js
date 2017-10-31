6903fd6c0d0af94748ccfd320ed1028d6f1acb05
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-10-30 19:52:42 -0600

    Style landing page - no data

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,9 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div>
-      <h2>DATA STUFF</h2>
-      <Link to="/auth"><h2>Sign In</h2></Link>
+    <div className="landing">
+      <h2 className="data-stuff">DATA STUFF</h2>
+      <button
+        className="enter-site-btn"
+      >
+        <Link
+          className="enter-site-link"
+          to="/auth"
+        >
+          Sign In
+        </Link>
+      </button>
     </div>
   )
 }
