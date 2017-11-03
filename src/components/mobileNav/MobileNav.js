c1a3c5bde789e24ca8521f48b5e2b915ee32934f
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-11-03 07:26:48 -0600

    Set up skelaton for MobileNav component

---
+++ src/components/mobileNav/MobileNav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../../index.css';
+import PropTypes from 'prop-types';
+
+const MobileNav = () => {
+
+  return (
+
+  );
+}
+
+MobileNav.propTypes = {
+
+};
+
+export default MobileNav;
