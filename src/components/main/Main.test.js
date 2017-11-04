ca1c9db7d4359dd3f4820271b5e8980c3d00b3b1
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-11-04 17:33:05 -0600

    Pass all but 1 main component test

--- src/components/main/Main.test.js
+++ src/components/main/Main.test.js
@@ -15,43 +15,43 @@ describe('<Main />', () => {
     expect(wrapper).toBeDefined();
   });
 
-  it.skip('should render a main section', () => {
+  it('should render a main section', () => {
     const main = wrapper.find('main');
 
     expect(main.length).toEqual(1);
   });
 
-  it.skip('should render three section titles', () => {
+  it('should render three section titles', () => {
     const sectionTitle = wrapper.find('h3');
 
     expect(sectionTitle.length).toEqual(3);
   });
 
-  it.skip('should render an emergency section', () => {
+  it('should render an emergency section', () => {
     const emergency = wrapper.find('.emergency-section');
 
     expect(emergency.length).toEqual(1);
   });
 
-  it.skip('should render an emergency button', () => {
+  it('should render an emergency button', () => {
     const emergencyBtn = wrapper.find('.emergency-btn');
 
     expect(emergencyBtn.length).toEqual(1);
   });
 
-  it.skip('should render a hotline section', () => {
+  it('should render a hotline section', () => {
     const hotline = wrapper.find('.hotline-section');
 
     expect(hotline.length).toEqual(1);
   });
 
-  it.skip('should render a hotline button', () => {
+  it('should render a hotline button', () => {
     const hotlineBtn = wrapper.find('.hotline-btn');
 
     expect(hotlineBtn.length).toEqual(1);
   });
 
-  it.skip(
+  it(
     'should render a "no contacts yet" card if the user has no contacts',
     () => {
       const noContacts = wrapper.find('.no-contacts-card');
@@ -59,10 +59,12 @@ describe('<Main />', () => {
       expect(noContacts.length).toEqual(1);
     });
 
-  it.skip('should run a function if the user does have contacts', () => {
+//does this no work bc its in the ternary inside the JSX? I gave it a prop of an empty array in the shallow, so IMO it should be working.
+  it('should run a function if the user does have contacts', () => {
     const contactsWrapper =
-    shallow(<Main loadedContacts={[{amy: '5756441355'}]}/>);
+    shallow(<Main loadedContacts={[]}/>);
 
+    wrapper.instance().noContacts = mockFn;
     expect(mockFn).toHaveBeenCalledTimes(1);
   });
 
