60f0b11344de1e38fc3c161c6412411a1a4a1470
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-11-04 08:07:01 -0600

    Write Main Container tests

---
+++ src/containers/tests/MainContainer.test.js
@@ -0,0 +1,37 @@
+import configureStore from 'redux-mock-store';
+import { mount } from 'enzyme';
+import MainContainer from '../MainContainer';
+import Main from '../../components/main/Main';
+import React from 'react';
+
+describe('Custom Message Form Container', () => {
+  const mockStore = configureStore();
+  const initialState = {
+    currentUser: '',
+    loadedContacts: [],
+  };
+  const mockLoadContacts = jest.fn();
+  const store = mockStore(initialState);
+  const wrapper = mount(<Main
+    store={store}
+    currentUser={initialState.currentUser}
+    loadedContacts={initialState.loadedContacts}
+    loadContacts={mockLoadContacts} />);
+
+  it('should have a default state', () => {
+    expect(wrapper.instance().props.currentUser).toEqual('');
+    expect(wrapper.instance().props.loadedContacts).toEqual([]);
+  });
+
+  it('should fire actions', () => {
+    const actionWrapper = mount(<Main
+      store={store}
+      currentUser={initialState.currentUser}
+      loadedContacts={initialState.loadedContacts}
+      loadContacts={mockLoadContacts} />);
+
+    //componentDidMount
+    expect(mockLoadContacts).toHaveBeenCalledTimes(1);
+  });
+
+});
