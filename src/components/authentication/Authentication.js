3253d5f2ac2aa9293c0756993674c6fbaced4e30
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-11-05 12:18:02 -0700

    Pass Auth Container tests - when Link and firebase is commented out

--- src/components/authentication/Authentication.js
+++ src/components/authentication/Authentication.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import firebase from '../../firebase';
 
